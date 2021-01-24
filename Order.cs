c3449751dffa1073661c5fe98ebf1d5346210a1a
MyriamBourahla <60229641+MyriamBourahla@users.noreply.github.com>
2021-01-24 18:55:52 +0100

    Add Orders.cs and OrderItem.cs

---
+++ Order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cours1
+{
+    class Order
+    {
+        // Constructeurs:
+        public Order()
+        {
+        
+        }
+
+        public Order(int orderId, DateTime orderDate)
+        {
+            OrderId = orderId;
+            OrderDate = orderDate;
+        }
+
+        // Champs:
+
+
+        // Proprietes:
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        //Methodes:
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+            
+        public bool Save()
+        {
+            return true;
+        }
+            
+       public bool Validate()
+        {
+            var isValid = true; 
+            if (OrderDate == null) isValid = false;
+            return isValid;
+        }
+
+
+    }
+}
---
+++ OrderItem.cs
@@ -0,0 +1,61 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cours1
+{
+    class OrderItem
+    {
+        // Constructeurs:
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int orderItemId, int productId , T purchasePrice, int quantity)
+        {
+            OrderItemId = orderItemId;
+            ProductId = productId;
+            PurchasePrice = purchasePrice;
+            Quantity = quantity;
+        }
+
+        //Champs:
+
+        //Propriétés:
+
+        public int OrderItemId { get; set; }
+
+        public int ProductId 
+        { 
+            get;
+            private set;
+        }
+        public T PurchasePrice { get; set; }
+        public int Quantity { get; set; }
+
+        //Méthodes:
+
+        public OrderItem Retrieve(int orderItemId)
+        {
+            return new OrderItem();
+        }
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if(Quantity>=0) isValid = false;
+            if (ProductId >= 0) isValid = false;
+            if (PurchasePrice != null) isValid = false;
+            return isValid;
+        }
+             
+    }
+}
+
+
